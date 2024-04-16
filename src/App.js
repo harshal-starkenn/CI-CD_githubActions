2e46f38e1b49799a159206ca0fb58fa0a8f43d42
harshal-starkenn <harshal@starkenn.com>
2024-04-16 13:04:38 +0530

    addedtext

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          Continuous Integration
+          Continuous Integration - Continuous Delivery !!
         </a>
       </header>
     </div>
