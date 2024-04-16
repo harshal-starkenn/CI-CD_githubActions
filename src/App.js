745a49d45c7bd9e408f81ce01840c3b7d031845e
harshal-starkenn <harshal@starkenn.com>
2024-04-16 12:55:05 +0530

    addedonci

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target='_blank'
           rel='noopener noreferrer'
         >
-          CI/CD Using GithubActions
+          Continuous Integration
         </a>
       </header>
     </div>
